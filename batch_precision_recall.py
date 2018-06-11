722174f20fe1160c4f67ecfbbab3914b9623d609
Andri Danusasmita <25168506+andridns@users.noreply.github.com>
2018-06-11 17:08:01 +0900

    Create batch_precision_recall.py

---
+++ batch_precision_recall.py
@@ -0,0 +1,30 @@
+def calc_precision_recall(img_results):
+    """Calculates precision and recall from the set of images
+
+    Args:
+        img_results (dict): dictionary formatted like:
+            {
+                'img_id1': {'true_pos': int, 'false_pos': int, 'false_neg': int},
+                'img_id2': ...
+                ...
+            }
+
+    Returns:
+        tuple: of floats of (precision, recall)
+    """
+    true_pos = 0; false_pos = 0; false_neg = 0
+    for _, res in img_results.items():
+        true_pos += res['true_pos']
+        false_pos += res['false_pos']
+        false_neg += res['false_neg']
+
+    try:
+        precision = true_pos/(true_pos + false_pos)
+    except ZeroDivisionError:
+        precision = 0.0
+    try:
+        recall = true_pos/(true_pos + false_neg)
+    except ZeroDivisionError:
+        recall = 0.0
+
+    return (precision, recall)
